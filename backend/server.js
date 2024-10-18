959e5f5068a163f8b1cd23b3caf985bf57fcf1ae
sivk-kex <adityamazumdar956@gmail.com>
2024-10-18 23:44:41 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const pool = require('./db');
 const loginRouter = require('./routes/login'); // Adjust path if needed
 
 const app = express();
 app.use(express.json()); // For parsing application/json
 app.use('/api', loginRouter); // Use the login router
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+pool.connect()
+    .then(()=>{
+        console.log(`Database connected successfully`);
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    })
+    .catch(err =>{
+        console.error('Database connection error: ', error);
+    })
